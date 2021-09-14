f8c1d14c75bd02637cfc9bcc7d30fbbd659d5607
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2021-09-14 21:33:04 +0200

    14/09/2021

---
+++ Dolaczanie_plikow.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>Poczatek strony</h3>
+    <?php
+      include '3_1.php';
+     ?>
+    <h3>Koniec strony</h3>
+  </body>
+</html>
