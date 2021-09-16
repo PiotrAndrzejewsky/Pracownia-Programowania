852ab1d717e1503f9bebec89d40d2b3cb32920b4
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2021-09-16 08:49:28 +0200

    16/09/2021

--- Dolaczanie_plikow.php
+++ Dolaczanie_plikow.php
@@ -8,6 +8,9 @@
     <h3>Poczatek strony</h3>
     <?php
       include '3_1.php';
+      include_once './3_1.php';
+      require './3_1.php';
+      require_once './3_1.php';
      ?>
     <h3>Koniec strony</h3>
   </body>
