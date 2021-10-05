b3a511f5dc8d0ae94e4508be39cdf1e05fee07e9
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2021-10-05 14:52:29 +0200

    05-10-2021

--- scripts/rectangle.php
+++ scripts/rectangle.php
@@ -7,9 +7,25 @@
   <body>
     <h3>Prostokąt</h3>
     <form method="get">
-      <input type="text" name="sideA" placeholder="Podaj długość boku a"> <br><br>
-      <input type="text" name="sideB" placeholder="Podaj długość boku b"> <br><br>
+      <input type="text" name="sideA" placeholder="Podaj długość boku A"><br><br>
+      <input type="text" name="sideB" placeholder="Podaj długość boku B"><br><br>
       <input type="submit" value="Oblicz">
     </form>
+    <?php
+      if (!empty($_GET['sideA']) && !empty($_GET['sideA'])){
+        //echo "pole = ", $_GET['sideA']**2, "cm^2";
+        $sideA=str_replace(',','.', $_GET['sideA']);
+        $sideA=str_replace(',','.', $_GET['sideB']);
+        $area=$sideA*$sideB;
+        $circuit=$sideA*2+$sideB*2;
+        echo <<< RESULT
+          <hr>
+          Pole prostokąta wynosi: $area cm<sup>2</sup><br>
+          Obwód prostokąta wynosi $circuit cm
+        RESULT;
+      }else(isset($_GET['button'])) {
+        echo "Wypełni wszystkie pola!";
+      }
+    ?>
   </body>
 </html>
--- scripts/square.php
+++ scripts/square.php
@@ -7,8 +7,24 @@
   <body>
     <h3>Kwadrat</h3>
     <form method="get">
-      <input type="text" name="sideA" placeholder="Podaj długość boku a"> <br><br>
+      <input type="text" name="sideA" placeholder="Podaj długość boku A"><br><br>
       <input type="submit" value="Oblicz">
     </form>
+    <?php
+      //if (!empty($_GET['sideA']) && is_numeric($_GET['sideA'])){
+      if (!empty($_GET['sideA'])){
+        //echo "pole = ", $_GET['sideA']**2, "cm^2";
+        $sideA=str_replace(',','.', $_GET['sideA']);
+        $area=$sideA**2;
+        $circuit=$sideA*4;
+        echo <<< RESULT
+          <hr>
+          Pole kwadratu wynosi: $area cm<sup>2</sup><br>
+          Obwód kwadratu wynosi $circuit cm
+        RESULT;
+      }else(isset($_GET['button'])) {
+        echo "Wypełni wszystkie pola!";
+      }
+    ?>
   </body>
 </html>
